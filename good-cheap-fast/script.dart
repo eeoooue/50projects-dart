c00d7d8ae950f3588f4abf971016fe7bb2fb5274
eeoooue <pdmoorhouse@gmail.com>
2023-07-22 04:09:40 +0100

    wrote up OO queue in dart in place of js implementation
    removing from the queue isn't ideal but I like the non-specific approach, this should generalise to many toggles

--- good-cheap-fast/script.dart
+++ good-cheap-fast/script.dart
@@ -1,36 +1,54 @@
 import 'dart:html';
+import 'dart:collection';
 
 class OptionManager {
   int limit = 0;
+  int active = 0;
+  final queue = Queue<OptionWidget>();
 
   OptionManager() {
     ElementList<Element> toggles = document.querySelectorAll('.toggle');
     limit = toggles.length;
 
     for (Element toggle in toggles) {
-      OptionWidget(this, toggle);
+      if (toggle is CheckboxInputElement) {
+        OptionWidget(this, toggle);
+      }
     }
+  }
+
+  void notify(OptionWidget widget) {
+    CheckboxInputElement checkbox = widget.element;
+    if (checkbox.checked == true) {
+      active += 1;
+      queue.add(widget);
+      if (active >= limit) {
+        deactivateOldest();
+      }
+    } else {
+      queue.remove(widget);
+      active -= 1;
+    }
+  }
+
+  void deactivateOldest() {
+    OptionWidget widget = queue.removeFirst();
+    if (widget.element.checked == true) {
+      widget.element.checked = false;
+      active -= 1;
+      return;
+    }
+    deactivateOldest();
   }
 }
 
 class OptionWidget {
-  Element element;
+  CheckboxInputElement element;
 
   OptionWidget(OptionManager manager, this.element) {
     element.addEventListener('change', (event) {
-      toggle();
+      manager.notify(this);
     });
-  }
-
-  void toggle() {
-    Element box = element;
-    if (box is CheckboxInputElement) {
-      if (box.checked != true) {
-        box.checked = true;
-      } else {
-        box.checked = false;
-      }
-    }
   }
 }
 
