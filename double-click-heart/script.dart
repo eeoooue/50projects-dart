c81afebcb79f84a849c9a5343a5fbfa21c60cfc1
eeoooue <pdmoorhouse@gmail.com>
2023-07-23 19:12:44 +0100

    created a single-click implementation (working)

---
+++ double-click-heart/script.dart
@@ -0,0 +1,48 @@
+import 'dart:html';
+import 'dart:async';
+
+class SocialElement {
+  int clickTime = 0;
+  int timesClicked = 0;
+
+  Element element;
+  SpanElement times;
+
+  SocialElement(this.element, this.times) {
+    element.addEventListener("click", (event) {
+      if (event is MouseEvent) {
+        Element heart = createHeart(event);
+        element.children.add(heart);
+        addClick();
+        new Timer(Duration(seconds: 1), () {
+          heart.remove();
+        });
+      }
+    });
+  }
+
+  void addClick() {
+    timesClicked += 1;
+    times.text = timesClicked.toString();
+  }
+
+  Element createHeart(MouseEvent event) {
+    Element heart = document.createElement("i");
+    heart.classes.add('fas');
+    heart.classes.add('fa-heart');
+
+    heart.style.top = "${event.offset.y}px";
+    heart.style.left = "${event.offset.x}px";
+
+    return heart;
+  }
+}
+
+void main() {
+  var loveMe = document.querySelector('.loveMe');
+  var times = document.getElementById('times');
+
+  if (loveMe is Element && times is SpanElement) {
+    SocialElement(loveMe, times);
+  }
+}
