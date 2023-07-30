186faa7f144091b55cc9477c6d7f10bdb31aa49a
eeoooue <pdmoorhouse@gmail.com>
2023-07-30 20:22:30 +0100

    introduced localstorage features

--- notes-app/script.dart
+++ notes-app/script.dart
@@ -19,43 +19,96 @@ class Notebook {
   List<NoteElement> notes = List.empty(growable: true);
 
   Notebook(this.addBtn) {
-    addBtn.addEventListener("click", (event) => createNote(""));
+    initialize();
+    addBtn.addEventListener("click", (event) => createNote("testing"));
   }
 
-  void initialize() {}
+  void initialize() {
+    String? data = window.localStorage[key];
 
-  void update() {}
+    print("attempted to initialize: data = ${data}");
+
+    if (data is String) {
+      List<String> noteStrings = unpackStrings(data);
+      for (String text in noteStrings) {
+        createNote(text);
+        print("attempted to create note with seed = ${text}");
+      }
+    }
+  }
+
+  List<String> unpackStrings(String list) {
+    List<String> result = List.empty(growable: true);
+
+    for (String note in list.split(",")) {
+      if (note.length > 1) {
+        result.add(note);
+      }
+    }
+
+    return result;
+  }
+
+  void update() {
+    print("notes is ${notes.length} long");
+
+    List<String> noteStrings = List.empty(growable: true);
+    for (NoteElement note in notes) {
+      noteStrings.add(note.text);
+      print("added ${note.text} to the list");
+    }
+
+    String data = encodeList(noteStrings);
+    window.localStorage[key] = data;
+
+    print("updated local storage");
+  }
+
+  String encodeList(List<String> strings) {
+    StringBuffer buffer = StringBuffer();
+
+    for (String s in strings) {
+      buffer.write(s);
+      buffer.write(",");
+    }
+
+    return buffer.toString();
+  }
 
   void createNote(String text) {
     var note = NoteElement(this, text);
-    document.body?.children.add(note.element);
+    notes.add(note);
+
+    print("added note -- notes is ${notes.length} long");
+    update();
   }
 
   void removeNote(NoteElement note) {
     this.notes.remove(note);
+    update();
   }
 }
 
 class NoteElement {
   Notebook parent;
   Element element = document.createElement('div');
-  String text = "";
+  String text;
 
   NoteElement(this.parent, this.text) {
     element.classes.add("note");
     setupElement();
+    document.body?.children.add(element);
   }
 
   void updateText() {
     var main = element.querySelector('.main');
-    var textArea = element.querySelector('textarea');
-
-    if (textArea is TextInputElement) {
-      textArea.value = text;
-    }
-
     if (main is Element) {
       main.innerText = text;
+    }
+
+    var textArea = element.querySelector('textarea');
+    if (textArea is TextInputElement) {
+      textArea.value = text;
     }
 
     parent.update();
@@ -87,14 +140,19 @@ class NoteElement {
 
     element.setInnerHtml(newHtml, validator: AllowAll());
 
+    addBtns();
+
     var textArea = element.querySelector('textarea');
 
     if (textArea is TextAreaElement) {
+      textArea.value = text;
       textArea.addEventListener("input", (event) {
         text = textArea.value!;
         updateText();
       });
     }
+
+    updateText();
   }
 
   void addBtns() {
